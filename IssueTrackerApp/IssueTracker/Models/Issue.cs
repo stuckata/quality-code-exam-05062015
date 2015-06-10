17dfb60af00b3243d9ccf089610443ebbc88cf1c
stuckata <stu.kirkov@gmail.com>
2015-06-10 15:12:41 +0300

    issue class

--- IssueTrackerApp/IssueTracker/Models/Issue.cs
+++ IssueTrackerApp/IssueTracker/Models/Issue.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IssueTracker.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,93 @@ namespace IssueTracker.Models
 {
     public class Issue
     {
-       
+        private int id = 0;
+        private string title;
+        private string description;
+        private Priority priorityType;
+        private IList<string> tags;
+        private IList<string> comments;
 
+        public Issue(string title, string description, Priority priorityType, IList<string> tags, IList<string> comments)
+        {
+            this.Id++;
+            this.Title = title;
+            this.Description = description;
+            this.PriorityType = priorityType;
+            this.Tags = tags;
+            this.Comments = comments;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                this.id = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return this.title;
+            }
+            set
+            {
+                this.title = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return this.description;
+            }
+            set
+            {
+                this.description = value;
+            }
+        }
+
+        public Priority PriorityType
+        {
+            get
+            {
+                return this.priorityType;
+            }
+            set
+            {
+                this.priorityType = value;
+            }
+        }
+
+        public IList<string> Tags
+        {
+            get
+            {
+                return this.tags;
+            }
+            set
+            {
+                this.tags = value;
+            }
+        }
+
+        public IList<string> Comments
+        {
+            get
+            {
+                return this.comments;
+            }
+            set
+            {
+                this.comments = value;
+            }
+        }
     }
 }
