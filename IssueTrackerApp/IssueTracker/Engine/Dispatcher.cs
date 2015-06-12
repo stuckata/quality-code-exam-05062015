dc358d0eb026a516133b634650795b2b0eb8fd44
stuckata <stu.kirkov@gmail.com>
2015-06-12 10:42:02 +0300

    dispatcher class

---
+++ IssueTrackerApp/IssueTracker/Engine/Dispatcher.cs
@@ -0,0 +1,31 @@
+﻿using IssueTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTracker.Engine
+{
+    public class Dispatcher
+    {
+        private Endpoint action;
+
+        public Dispatcher(Endpoint action)
+        {
+            this.Action = action;
+        }
+
+        public Endpoint Action
+        {
+            get
+            {
+                return this.action;
+            }
+            set
+            {
+                this.action = value;
+            }
+        }
+    }
+}
---
+++ IssueTrackerApp/IssueTracker/Models/Endpoint.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssueTracker.Models
+{
+    public class Endpoint
+    {
+        private string actionName;
+        private string[] parameters;
+
+        public Endpoint(string actionName, string[] parameters)
+        {
+            this.ActionName = actionName;
+            this.Parameters = parameters;
+        }
+
+        public string ActionName
+        {
+            get
+            {
+                return this.actionName;
+            }
+            set
+            {
+                this.actionName = value;
+            }
+        }
+
+        public string[] Parameters
+        {
+            get
+            {
+                return this.parameters;
+            }
+            set
+            {
+                this.parameters = value;
+            }
+        }
+    }
+}
